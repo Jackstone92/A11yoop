0b501b3d5c59510e99f29504b8e1992ae4e84ed6
Jack <jackstonemedia@gmail.com>
2021-10-20 18:18:54 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,26 +3,74 @@
 
 import PackageDescription
 
-let package = Package(
+// MARK: - Main
+var package = Package(
     name: "A11yoop",
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "A11yoop",
             targets: ["A11yoop"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "A11yoop",
-            dependencies: []),
+            dependencies: ["A11yCore"]),
+        .target(
+            name: "A11yoopLive",
+            dependencies: ["A11yoop", "A11yStatusManagerLive", "A11yStatusEmitterLive", "A11yFeaturesLive"]),
         .testTarget(
             name: "A11yoopTests",
-            dependencies: ["A11yoop"]),
+            dependencies: ["A11yoopLive"]),
     ]
 )
+
+// MARK: - Common
+package.targets.append(contentsOf: [
+    .target(
+        name: "A11yCore",
+        dependencies: []),
+])
+
+// MARK: - Features
+package.targets.append(contentsOf: [
+    .target(
+        name: "A11yFeaturesLive",
+        dependencies: ["A11yCore"]),
+])
+
+// MARK: - Clients
+package.targets.append(contentsOf: [
+    .target(
+        name: "A11yStatusEmitter",
+        dependencies: ["A11yCore"]),
+    .target(
+        name: "A11yStatusEmitterLive",
+        dependencies: ["A11yStatusEmitter"]),
+    .testTarget(
+        name: "A11yStatusEmitterTests",
+        dependencies: ["A11yStatusEmitterLive"]),
+
+    .target(
+        name: "A11yStore",
+        dependencies: ["A11yCore"]),
+    .target(
+        name: "A11yStoreLive",
+        dependencies: ["A11yStore"]),
+
+    .target(
+        name: "A11yStatusObserver",
+        dependencies: ["A11yCore", "A11yStatusEmitter", "A11yStoreLive"]),
+    .target(
+        name: "A11yStatusObserverLive",
+        dependencies: ["A11yStatusObserver"]),
+
+    .target(
+        name: "A11yStatusManager",
+        dependencies: ["A11yCore", "A11yStatusEmitter"]),
+    .target(
+        name: "A11yStatusManagerLive",
+        dependencies: ["A11yStatusManager", "A11yStatusObserverLive", "A11yStoreLive"]),
+])
