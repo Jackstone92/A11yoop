6dbc5396da0d0a89c864d5590a7509754f1ef075
Jack <jackstonemedia@gmail.com>
2021-10-20 18:25:29 +0100

    Add A11yoop interface

---
+++ Sources/A11yoop/Interface.swift
@@ -0,0 +1,16 @@
+//
+//  Interface.swift
+//  Copyright © 2021 Notonthehighstreet Enterprises Limited. All rights reserved.
+//
+
+import Foundation
+import A11yCore
+
+public struct A11yoop {
+
+    public let isFeatureEnabled: (_ feature: A11yFeatureType) -> Bool
+
+    public init(isFeatureEnabled: @escaping (A11yFeatureType) -> Bool) {
+        self.isFeatureEnabled = isFeatureEnabled
+    }
+}
