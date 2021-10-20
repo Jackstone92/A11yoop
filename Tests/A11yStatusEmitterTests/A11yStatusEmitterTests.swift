c54277533602c8fabcb9e6760fd5dac4895ca08c
Jack <jackstonemedia@gmail.com>
2021-10-20 18:26:31 +0100

    Add tests TBC

---
+++ Tests/A11yStatusEmitterTests/A11yStatusEmitterTests.swift
@@ -0,0 +1,10 @@
+//
+//  A11yStatusEmitterTests.swift
+//  Copyright © 2021 Notonthehighstreet Enterprises Limited. All rights reserved.
+//
+
+import XCTest
+
+final class A11yStatusEmitterTests: XCTestCase {
+
+}
