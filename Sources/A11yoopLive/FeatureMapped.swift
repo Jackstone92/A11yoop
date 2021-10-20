c6b1d45ad959106ecacd299bcf7c8afed581e59b
Jack <jackstonemedia@gmail.com>
2021-10-20 18:25:35 +0100

    Add A11yoopLive

---
+++ Sources/A11yoopLive/FeatureMapped.swift
@@ -0,0 +1,29 @@
+//
+//  FeatureMapped.swift
+//  Copyright © 2021 Notonthehighstreet Enterprises Limited. All rights reserved.
+//
+
+import Foundation
+import A11yCore
+
+struct FeatureMapped<T, U> {
+    let mapped: (T) -> U
+}
+
+extension FeatureMapped where T == A11yFeatureType, U == A11yFeature {
+
+    static var asA11yFeature: Self {
+        Self { featureType in
+            switch featureType {
+            case .voiceOver: return .voiceOver
+            }
+        }
+    }
+}
+
+extension Sequence where Element == A11yFeatureType {
+
+    func map<T>(_ featureMapped: FeatureMapped<Element, T>) -> [T] {
+        return self.map { featureMapped.mapped($0) }
+    }
+}
---
+++ Sources/A11yoopLive/Live.swift
@@ -0,0 +1,32 @@
+//
+//  Live.swift
+//  Copyright © 2021 Notonthehighstreet Enterprises Limited. All rights reserved.
+//
+
+import Foundation
+import A11yCore
+import A11yoop
+import A11yStatusEmitter
+import A11yStatusEmitterLive
+import A11yStatusManager
+import A11yStatusManagerLive
+import A11yFeaturesLive
+
+extension A11yoop {
+
+    public static func live(
+        featureTypes: [A11yFeatureType] = A11yFeatureType.allCases,
+        statusManager: A11yStatusManager = .live(),
+        emitter: A11yStatusEmitter = .log
+    ) -> Self {
+
+        let features = featureTypes.map(.asA11yFeature)
+        statusManager.observeFeatures(features, emitter)
+
+        return Self(
+            isFeatureEnabled: { featureType in
+                statusManager.isFeatureEnabled(featureType)
+            }
+        )
+    }
+}
