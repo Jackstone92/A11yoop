4f1e3c4324e49f5f5991300a598e9c7bb35dbb31
Jack <jackstonemedia@gmail.com>
2021-10-20 18:24:52 +0100

    Add A11yStatusManager interface

---
+++ Sources/A11yStatusManager/A11yStatusManager.swift
@@ -0,0 +1,22 @@
+//
+//  A11yStatusManager.swift
+//  Copyright © 2021 Notonthehighstreet Enterprises Limited. All rights reserved.
+//
+
+import Foundation
+import A11yCore
+import A11yStatusEmitter
+
+public struct A11yStatusManager {
+
+    public let observeFeatures: (_ features: [A11yFeature], _ emitter: A11yStatusEmitter) -> Void
+    public let isFeatureEnabled: (_ feature: A11yFeatureType) -> Bool
+
+    public init(
+        observeFeatures: @escaping (_ features: [A11yFeature], _ emitter: A11yStatusEmitter) -> Void,
+        isFeatureEnabled: @escaping (_ feature: A11yFeatureType) -> Bool
+    ) {
+        self.observeFeatures = observeFeatures
+        self.isFeatureEnabled = isFeatureEnabled
+    }
+}
