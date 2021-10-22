091c7c9e8b78d1d02874022a6910e92b7ba50c7d
Jack <jackstonemedia@gmail.com>
2021-10-22 07:01:02 +0100

    Add A11yStatus+DescriptionTests

---
+++ Tests/A11yFeatureTests/A11yStatus+DescriptionTests.swift
@@ -0,0 +1,22 @@
+//
+//  A11yStatus+DescriptionTests.swift
+//  Copyright © 2021 Notonthehighstreet Enterprises Limited. All rights reserved.
+//
+
+import XCTest
+import A11yFeature
+
+final class A11yStatus_DescriptionTests: XCTestCase {
+
+    private typealias sut = A11yStatus
+
+    func test_descriptionForEnabled() {
+
+        XCTAssertEqual(sut.enabled.description, "Enabled")
+    }
+
+    func test_descriptionForDisabled() {
+
+        XCTAssertEqual(sut.disabled.description, "Disabled")
+    }
+}
