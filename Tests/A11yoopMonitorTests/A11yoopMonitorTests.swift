5c4a84c67c158e9eee531cd0c4b6793865e0ec62
Jack <jackstonemedia@gmail.com>
2021-10-31 19:44:18 +0000

    Fix tests not compiling

---
+++ Tests/A11yoopMonitorTests/A11yoopMonitorTests.swift
@@ -0,0 +1,11 @@
+//
+//  A11yoopMonitorTests.swift
+//  Copyright © 2021 Notonthehighstreet Enterprises Limited. All rights reserved.
+//
+
+import XCTest
+@testable import A11yoopMonitor
+
+final class A11yoopMonitorTests: XCTestCase {
+
+}
--- Tests/A11yoopMonitorTests/A11yoopTests.swift
+++
@@ -1,6 +0,0 @@
-import XCTest
-@testable import A11yoop
-
-final class A11yoopTests: XCTestCase {
-
-}
