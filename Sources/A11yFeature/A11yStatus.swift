8f442b7314656bd2fda3623a6b6236a90d7dede7
Jack <jackstonemedia@gmail.com>
2022-01-13 18:31:14 +0000

    Add new notMonitored status and description

--- Sources/A11yFeature/A11yStatus.swift
+++ Sources/A11yFeature/A11yStatus.swift
@@ -19,16 +19,20 @@ public enum A11yStatus: Equatable {
 
     /// The accessibility feature is not supported on the current device.
     case notSupported
+
+    /// The accessibility feature is not currently being monitored
+    case notMonitored
 }
 
 extension A11yStatus: CustomStringConvertible {
 
     public var description: String {
         switch self {
-        case .enabled:  return "Enabled"
-        case .disabled: return "Disabled"
-        case .contentSize(let size): return size.description
-        case .notSupported: return "Not Supported"
+        case .enabled:                  return "Enabled"
+        case .disabled:                 return "Disabled"
+        case .contentSize(let size):    return size.description
+        case .notSupported:             return "Not Supported"
+        case .notMonitored:             return "Not Monitored"
         }
     }
 }
